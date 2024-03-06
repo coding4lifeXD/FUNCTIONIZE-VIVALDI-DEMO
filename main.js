f9195c735e33d94c7001dd739481a5218d25f3bf
coding4lifeXD <158625345+coding4lifeXD@users.noreply.github.com>
2024-03-06 14:07:23 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// Functionize Vivaldi Demo
+
+// spring-color: #0E94D1
+// summer-color: #1BA848
+// autumn-color: #FE6732
+// winter-color: #1C64B9
+
+// Variables for HTML Elements
+let seasonSelect = document.getElementById("season-select");
+let seasonH2 = document.getElementById("season-heading");
+let seasonImg = document.getElementById("season-img");
+let seasonAudio = document.getElementById("season-audio");
+
+// Event Listener
+seasonSelect.addEventListener("input", seasonSelectChanged);
+
+// Event handler for season select
+function seasonSelectChanged() {
+  // Get selected season
+  let season = seasonSelect.value;
+
+  // Update page to match selected season
+  if (season === "spring") {
+    setSeason("spring", "#0E94D1");
+  } else if (season === "summer") {
+    setSeason("summer", "#1BA848");
+  } else if (season === "autumn") {
+    setSeason("autumn", "#FE6732");
+  } else if (season === "winter") {
+    setSeason("winter", "#1C64B9");
+  }
+}
+
+// BENIFIT - MODULARITY / ORGANIZATION
+// BENIFIT - EFFICIENCY / REUSABILITY
+
+// parameters: variables that store data passed into the function
+function setSeason(seasonName, seasonColor) {
+  seasonH2.innerHTML = seasonName;
+  seasonH2.style.color = seasonColor;
+  seasonImg.src = `img/${seasonName}.jpg`;
+  seasonAudio.src = `songs/vivaldi-${seasonName}.mp3`;
+  document.body.style.backgroundColor = seasonColor;
+}
